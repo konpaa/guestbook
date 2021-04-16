5d18733eca8c7f064a33d8e1fd1c9162a4198cfb
Konpa <pavelkonoplyanikov@yandex.by>
2021-04-16 11:55:18 +0300

    createAt now, and slug

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Comment
 {
@@ -109,6 +110,14 @@ class Comment
     public function getConference(): ?Conference
     {
         return $this->conference;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function setCreatedAtValue()
+    {
+        $this->createdAt = new \DateTime();
     }
 
     public function setConference(?Conference $conference): self
